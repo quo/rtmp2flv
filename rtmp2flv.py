1a3fce94c0c508107b5678f2213b874e96e5227d
quo <tuple@list.ru>
2013-12-25 19:59:33 +0100

    Add support for AMF3 messages that use only AMF0 mode

--- rtmp2flv.py
+++ rtmp2flv.py
@@ -162,10 +162,13 @@ def read_rtmp_messages(f, chunksize):
 				pass
 			elif s.type in (8,9): # audio/video
 				if data: yield Message(timestamp=s.timestamp, type=s.type, streamid=s.streamid, data=data)
-			elif s.type == 18:
-				log.info('Stream %i AMF0 data: %r', s.streamid, AMF0.parse(data))
-			elif s.type == 20:
-				log.info('Stream %i AMF0 command: %r', s.streamid, AMF0.parse(data))
+			elif s.type in (15,17,18,20): # AMF
+				version = 3 if s.type < 18 else 0
+				if version == 3:
+					if data[0]: log.error('AMF3: expected 0, got %i', data[0])
+					data = data[1:]
+				log.info('Stream %i AMF%i %s: %r', s.streamid, version,
+					'data' if s.type in (15,18) else 'command', AMF.parse(data))
 			elif s.type == 22: # aggregate
 				g = io.BytesIO(data)
 				firstts = None
@@ -182,15 +185,15 @@ def read_rtmp_messages(f, chunksize):
 					else: log.warn('Non-AV message type (%i) inside aggregate message', msgtype)
 			else: log.debug('Unhandled message type %i', s.type)
 
-class AMF0:
+class AMF:
 	def parse(data):
 		f = io.BytesIO(data)
 		decoded = []
 		objects = []
 		try:
-			while f.tell() < len(data): decoded.append(AMF0.read(f, objects))
+			while f.tell() < len(data): decoded.append(AMF.read(f, objects))
 		except Exception:
-			log.exception('Error decoding AMF0 data')
+			log.exception('Error decoding AMF data')
 		return decoded
 
 	def read_str(f, sz):
@@ -200,30 +203,30 @@ class AMF0:
 		t, = f.read(1)
 		if t == 0: return read(f, '>d')[0]
 		if t == 1: return bool(*f.read(1))
-		if t == 2: return AMF0.read_str(f, '>H')
+		if t == 2: return AMF.read_str(f, '>H')
 		if t in (3,8,16):
 			if t == 8: f.read(4)
-			if t == 16: cls = AMF0.read_str(f, '>H')
+			if t == 16: cls = AMF.read_str(f, '>H')
 			obj = {}
 			while True:
-				name = AMF0.read_str(f, '>H')
-				val = AMF0.read(f, objects)
+				name = AMF.read_str(f, '>H')
+				val = AMF.read(f, objects)
 				if val is StopIteration: break
 				obj[name] = val
 			if t == 16: obj = cls, obj
 			objects.append(obj)
 			return obj
-		if t == 4: return 'MovieClip', AMF0.read_str(f, '>H')
+		if t == 4: return 'MovieClip', AMF.read_str(f, '>H')
 		if t in (5,6): return None
 		if t == 7: return objects[read(f, '>H')[0]]
 		if t == 9: return StopIteration
 		if t == 10:
-			array = [AMF0.read(f, objects) for _ in range(*read(f, '>I'))]
+			array = [AMF.read(f, objects) for _ in range(*read(f, '>I'))]
 			objects.append(array)
 			return array
 		if t == 11: return ('Date',) + read(f, '>dH')
-		if t == 12: return AMF0.read_str(f, '>I')
-		if t == 15: return 'XMLDocument', AMF0.read_str(f, '>I')
+		if t == 12: return AMF.read_str(f, '>I')
+		if t == 15: return 'XMLDocument', AMF.read_str(f, '>I')
 		raise Exception('Unhandled type %i at 0x%x' % (t, f.tell()-1))
 
 if __name__ == '__main__': main(parser.parse_args())
